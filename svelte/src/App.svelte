9d19360ec4ab64baaf6a458876aabf61ca2017ce
Mariana Costa <mariana.costa214@gmail.com>
2021-10-04 19:15:50 +0100

    Add basic styling and intro draft

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,30 +1,49 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h2 class="header-title">Mariana Costa</h2>
+	<hr/>
+	<div class="intro">
+		<ul class="intro-list">
+			<li>Hi! I’m Mariana Costa, a recent graduate from FEUP’s BSc + MSc in Informatics and Computer Engineering.</li>
+			<li>Most familiar with web development, but keen on exploring other areas of software engineering, such as software development.</li>
+			<li>In my free time, I usually cycle between playing my Switch (currently struggling at Slay the Spire), reading, and scouring cafés in the city.</li>
+			<li>You can take a look at my CV or download it!</li>
+		</ul>
+	</div>
+	<div>
+		<h3 class="content-title">Projects</h3>
+	</div>
+	<div class="footer">
+		<a href="https://github.com/marianafcosta">GitHub</a>
+		<a href="https://www.linkedin.com/in/marianafcosta/">LinkedIn</a>
+	</div>
 </main>
 
 <style>
 	main {
-		text-align: center;
 		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		max-width: 800px;
+		margin: auto;
 	}
-
-	h1 {
+	.header-title {
+		margin: 0;
+	}
+	.header-title, .content-title {
 		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
 		font-weight: 100;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.intro {
+		margin-top: 32px;
+		border: 1px solid black;
+		padding: 16px;
+	}
+	.intro-list {
+		margin: 0;
+	}
+	.footer {
+		margin-top: 32px;
+		text-align: center;
 	}
 </style>
